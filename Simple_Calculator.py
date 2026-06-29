607754d46272fb934c09260131d4e1686ffb11ab
farvez4-tech <farvezmusaraf4@gmail.com>
2026-06-29 19:36:53 +0530

    Add files via upload

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,20 +1,30 @@
-print("------Simple Calculator------")
-print("1.  Addition")
-print("2.  Subtraction")
-print("3.  Multiplication")
-print("4.  Divition")
-choice = input("Enter yiur choice (1 - 4):  ")
+ 
 try:
-    num1 = float(input("Enter first Number:  "))
-    num2 = float(input("Enter second Number:  "))
-    if choice == "1":
-        print(f"Answer = {num1 + num2}")
-    elif choice == "2":
-        print(f"Answer = {num1 - num2}")
-    elif choice == "3":
-        print(f"Answer = {num1 *  num2}")
-    elif choice == "4":
-        print(f"Answer = {num1 / num2}")
+    while True:
+        print("------Simple Calculator------")
+        print("1.  Addition")
+        print("2.  Subtraction")
+        print("3.  Multiplication")
+        print("4.  Divition")
+        print("5.  Exit")
+        choice = input("Enter your choice (1 - 4):  ")
+        
+        
+        num1 = float(input("Enter first Number:  "))
+        num2 = float(input("Enter second Number:  "))
+        if choice == "1":
+                print(f"Answer = {num1 + num2}")
+        elif choice == "2":
+                print(f"Answer = {num1 - num2}")
+        elif choice == "3":
+                print(f"Answer = {num1 *  num2}")
+        elif choice == "4":
+                print(f"Answer = {num1 / num2}")
+        again = input("Do you want to continue ? (yes / no)")
+        if again.lower() == "no":
+                print("Thank you for using the calculator!")
+                break
+                
 except ValueError:
         print("Please enter a Integer Value!!!")
 except ZeroDivisionError:
